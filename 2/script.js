5fa02b2c80e0ea0540830cdf812de2012b23a068
XanderGale <alessandro.galeazzi@icloud.com>
2021-11-19 16:19:11 +0100

    correzione lista invitati

--- 2/script.js
+++ 2/script.js
@@ -1,20 +1,21 @@
 const nomi = ['Alberto', 'Luigi', 'Luca'];
 const cognomi = ['De Luca', 'Escobar', 'Ratti'];
-const listaInvitati = parseInt(prompt('Inserisci Numero Invitati'));
-
-
+const numeroInvitati = parseInt(prompt('Inserisci Numero Invitati'));
+const listaInvitati = [];
 
 // console.log(nomi[randomNome]);
 
 // console.log(cognomi[randomCognome]);
 
-for ( i = 0; i < listaInvitati; i++ ){
+for ( i = 0; i < numeroInvitati; i++ ){
     let randomCognome = Math.floor(Math.random() * (cognomi.length - 1 + 1));
     let randomNome = Math.floor(Math.random() * (nomi.length - 1 + 1) );
     let outputString = `${nomi[randomNome]} ${cognomi[randomCognome]}`;
-    alert(outputString);
+
+    listaInvitati.push(' ' + outputString);
 }
 
+alert(listaInvitati);
 
 
 
